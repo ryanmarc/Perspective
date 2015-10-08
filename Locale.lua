ed90eec95524bfd7f414ab22e9009acd8fcc60dd
Soey <soey.flamepaw@gmail.com>
2015-10-08 18:17:28 +0200

    Repeatable Quests
    - Fixed Tracking for Repeatable Quests
    - Added Icon for Repeatable Quests

--- Locale.lua
+++ Locale.lua
@@ -54,6 +54,7 @@ function PerspectiveLocale:LoadLocalization()
     locale.Category_Quest_Interactable              = "Interactable"
     locale.Category_Quest_Start                     = "Start"
     locale.Category_Quest_Start_Main                = "Start (Main)"
+    locale.Category_Quest_Start_Repeatable          = "Start (Repeatable)"
     locale.Category_Quest_TalkTo                    = "Talk To"
     locale.Category_Quest_Complete                  = "Complete"
     locale.Category_Quest_Location                  = "Quest Location"
